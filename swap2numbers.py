66577f0148e5af36eb2c99f9e17a3784e6b09e93
dev1 <dev1@gmail.com>
2019-12-19 00:35:14 +0530

    i added swap2numbers.py

---
+++ swap2numbers.py
@@ -0,0 +1,19 @@
+#with third varriable using
+a=3
+b=5
+temp = a
+a = b
+b = temp
+print("after swaping")
+print(a)
+print(b)
+
+#with out third variable
+a=6
+b=8
+a=a+b
+b=a-b
+a=a-b
+print("after swaping")
+print(a)
+print(b)
