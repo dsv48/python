c917ff908dd111084999292d307466c97f1cb747
dev1 <dev1@gmail.com>
2019-12-19 00:14:14 +0530

    i added fibonnicwithfor.py

---
+++ fibonnicwithwhile.py
@@ -0,0 +1,16 @@
+
+n = int(input("enter any number? "))
+n1 = 0
+n2 = 1
+count = 0
+
+if n <= 0:
+   print("Please enter a positive integer")
+else:
+   print("Fibonacci sequence:")
+   while count < n:
+       print(n1)
+       nth = n1 + n2
+       n1 = n2
+       n2 = nth
+       count += 1
