759ff5689ff351b40d810af450b442b7dd3b20d7
dev1 <dev1@gmail.com>
2019-12-19 00:34:05 +0530

    i added stringmethods.py

---
+++ stringmethods.py
@@ -0,0 +1,21 @@
+#count
+str="srinivasgoud"
+print(str.count("s"))
+#slicing
+print(str[3])
+print(str[-3])
+print(str[3:6])
+#indexinx
+print(str.index("n"))
+#split
+str="srinivas parepalli goud"
+print(str.split("goud"))
+#upper
+str = "srinivas parepalli"
+print(str.upper())
+#lower
+str="SRINIVAS PAREPALLI"
+print(str.lower())
+#title
+str="srinivas parepalli goud"
+print(str.title())
