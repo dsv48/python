0b2db19a1e036b4c0c0bce9931b9020d6cf44973
dev1 <dev1@gmail.com>
2019-12-19 00:30:34 +0530

    i added pythonarithmeticoperators.py

---
+++ pythonarithmeticoperators.py
@@ -0,0 +1,14 @@
+a=int(input("enter any number"))
+b=int(input("enter any number"))
+add=a+b
+sub=a-b
+div=a/b
+truncatingdiv=a//b
+mod=a%b
+multi=a*b
+print(add)
+print(sub)
+print(div)
+print(truncatingdiv)
+print(mod)
+print(multi)
