3418ceb834ce261e1d88fec79464de8f409eeeb9
dev1 <dev1@gmail.com>
2019-12-19 00:28:52 +0530

    i added primenousingwhile.py

---
+++ primenousingwhile.py
@@ -0,0 +1,10 @@
+n=int(input("enter any positive number"))
+i=2
+while i<=n:
+    if n%i==0:
+
+        print("n is not a prime nuber")
+    else:
+        print("n is a prime number")
+        i=i+1
+
