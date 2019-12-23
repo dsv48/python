9a655c369415c3e52f41a725964e9b872b286d5a
dev1 <dev1@gmail.com>
2019-12-23 23:59:14 +0530

    i added new electricitybill.py

--- electricitybill.py
+++ electricitybill.py
@@ -8,18 +8,18 @@ if (units <= 50):
     surcharge = 25
 
 #above 50 units first take the 50 units charge next subtract the 50 units from given units
-elif (units <= 100):
+elif (units>50 and units <= 100):
     amount = (50*2) + ((units - 50) * 3)
     surcharge = 35
 
 #above 100 units first take 50 and 100 units charge next subtract the 100 units from given units
-elif (units <= 200):
-    amount = (50*2) +(100*3)  + ((units - 100) * 5)
+elif (units>100 and units <= 200):
+    amount = (50*2) +((100-50)*3)  + ((units - 100) * 5)
     surcharge = 45
 
 #above 200 units first take the 50 and 100 and 200 units charge and next subtract the 200 units from given units
 else:
-    amount = (50*2) + (100*3) +(100*5) + ((units - 200) * 8)
+    amount = (50*2) + ((100-50)*3) +((200-100)*5) + ((units - 200) * 8)
     surcharge = 75
 
 total = amount + surcharge
