d9b3f46c2b998582529b47c5c5a59986551e1583
dev1 <dev1@gmail.com>
2019-12-24 00:07:15 +0530

    i aded  loopsbreakandcontinue.py

---
+++ loopsbreakandcontinue.py
@@ -0,0 +1,17 @@
+# Use of break statement inside loop
+for val in "string":
+    if val == "i":
+        break
+    print(val)
+
+print("The end")
+
+# Use of continue statement inside loops
+
+for val in "string":
+    if val == "i":
+        continue
+    print(val)
+
+print("The end")
+
